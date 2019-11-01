0c31e96e33e98db8a920b27a11b3b152cee38834
bannzai <kingkong999yhirose@gmail.com>
2019-11-02 00:52:48 +0900

    :sparkels: Hello world

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println("Start process")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello World")
+	})
+	log.Fatal(http.ListenAndServe(":5000", nil))
 }
