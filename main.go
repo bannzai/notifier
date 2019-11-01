43c2996f3c9469152293f8e60b1d364e7c360792
bannzai <kingkong999yhirose@gmail.com>
2019-11-02 01:15:22 +0900

    :sparkles: setting port

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
+const defaultPort = "5000"
+
 func main() {
+	port := os.Getenv("PORT")
+
+	if len(port) == 0 {
+		port = defaultPort
+	}
+
 	fmt.Println("Start process")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World")
 	})
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
