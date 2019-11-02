8bc4e5ec7e2fe8795059f0b4721f8720287a65f0
bannzai <kingkong999yhirose@gmail.com>
2019-11-02 13:44:32 +0900

    :sparkles: Add driver method

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -5,3 +5,8 @@ import "net/http"
 type Driver interface {
 	Drive(r *http.Request) error
 }
+
+func Drive(r *http.Request, drivers ...Driver) error {
+	var driver Drivers = drivers
+	return driver.Drive(r)
+}
