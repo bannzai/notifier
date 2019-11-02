2404c228d445aaef8f692e70b5b4f6921d1b4abd
bannzai <kingkong999yhirose@gmail.com>
2019-11-03 01:41:02 +0900

    :sparkles: add test for github comment parser

--- pkg/parser/github_test.go
+++ pkg/parser/github_test.go
@@ -1,54 +1,11 @@
 package parser
 
 import (
-	"net/http"
 	"reflect"
 	"testing"
+
+	"github.com/bannzai/notifier/pkg/testutil"
 )
-
-func TestNewGitHub(t *testing.T) {
-	tests := []struct {
-		name string
-		want GitHub
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewGitHub(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewGitHub() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func TestGitHub_Parse(t *testing.T) {
-	type args struct {
-		request *http.Request
-	}
-	tests := []struct {
-		name    string
-		parser  GitHub
-		args    args
-		want    Content
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			parser := GitHub{}
-			got, err := parser.Parse(tt.args.request)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GitHub.Parse() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GitHub.Parse() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestGitHub_parseBody(t *testing.T) {
 	type args struct {
@@ -61,7 +18,18 @@ func TestGitHub_parseBody(t *testing.T) {
 		want    Content
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "parse successfully when read truth json",
+			g:    GitHub{},
+			args: args{
+				body: testutil.ReadFile(t, testutil.CallerDirectoryPath(t)+"/testdata/github_comment.json"),
+			},
+			want: Content{
+				LinkURL:     "https://github.com/bannzai/notifier/pull/1#issuecomment-549011949",
+				UserNames:   []string{"bannzai"},
+				ContentType: GitHubContent,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
