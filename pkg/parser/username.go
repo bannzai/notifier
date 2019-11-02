14b262091d01190c3ac6ef327c95e0dfdde86aee
bannzai <kingkong999yhirose@gmail.com>
2019-11-03 01:20:04 +0900

    :sparkles: Fix test for parse usernames

--- pkg/parser/username.go
+++ pkg/parser/username.go
@@ -1,10 +1,18 @@
 package parser
 
-import "regexp"
+import (
+	"regexp"
+)
 
 func userNames(target string) []string {
-	regex := regexp.MustCompile(`@(\S+)\s+`)
-	matched := regex.FindAllStringSubmatch(target, -1)[0]
-	usernames := matched[1 : len(matched)-1]
+	regex := regexp.MustCompile(`@(\S+)\s*`)
+	matches := regex.FindAllStringSubmatch(target, -1)
+	if len(matches) == 0 {
+		return []string{}
+	}
+	usernames := []string{}
+	for _, matched := range matches {
+		usernames = append(usernames, matched[1])
+	}
 	return usernames
 }
