c90c42ce83c414d30d9e67baf24ad00edb34d1ad
bannzai <kingkong999yhirose@gmail.com>
2019-11-05 01:00:11 +0900

    :sparekls: Add method of extract other id

--- pkg/parser/content.go
+++ pkg/parser/content.go
@@ -4,10 +4,20 @@ type ContentType int
 
 const (
 	GitHubContent ContentType = iota
+	SlackContent
 )
 
 type Content struct {
 	LinkURL   string
 	UserNames []string
 	ContentType
+}
+
+func (content Content) ExtractID(targetContentType ContentType) string {
+	m := content.extractIDMap(content.ContentType)
+	return m[targetContentType]
+}
+
+func (content Content) extractIDMap(fromContentType ContentType) map[ContentType]string {
+	return map[ContentType]string{}
 }
