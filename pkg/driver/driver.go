2b21cc799e954ae3c445e695417eed95a3964f93
bannzai <kingkong999yhirose@gmail.com>
2019-11-05 18:37:59 +0900

    :sparkles: Add driver impl

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -1,9 +1,26 @@
 package driver
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/pkg/errors"
+)
 
 type Driver interface {
 	Drive(r *http.Request) error
+}
+
+type DriverImpl struct {
+	Parser
+	Sender
+}
+
+func (driver DriverImpl) Drive(r *http.Request) error {
+	content, err := driver.Parser.Parse(r)
+	if err != nil {
+		return errors.Wrapf(err, "driver.Parser.Parse is failed. request is %v", r)
+	}
+	return driver.Send(content)
 }
 
 func Drive(r *http.Request, drivers ...Driver) error {
