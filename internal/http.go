d50e8783093f61f5fca5440adc6832f19792cafb
bannzai <kingkong999yhirose@gmail.com>
2019-11-05 18:44:33 +0900

    :recycle: Using github to slack driver

--- internal/http.go
+++ internal/http.go
@@ -3,8 +3,11 @@ package internal
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/bannzai/notifier/pkg/driver"
+	"github.com/bannzai/notifier/pkg/parser"
+	"github.com/bannzai/notifier/pkg/sender"
 )
 
 func GitHub(w http.ResponseWriter, r *http.Request) {
@@ -16,7 +19,9 @@ func GitHub(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := driver.NewGitHub().Drive(r); err != nil {
+	// TODO: Flexing with command line argument
+	githubToSlackDriver := driver.New(parser.NewGitHub(), sender.NewSlack(os.Getenv("NOTIFIER_SLACK_TOKEN")))
+	if err := githubToSlackDriver.Drive(r); err != nil {
 		log.Printf("GitHub driver error with %v", err)
 	}
 }
