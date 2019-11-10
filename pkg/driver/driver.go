531aaae1f6599aa21b8152e81bd931d0403094ab
bannzai <kingkong999yhirose@gmail.com>
2019-11-10 11:11:55 +0900

    :recycle: Unnecessary interface

--- pkg/driver/driver.go
+++ pkg/driver/driver.go
@@ -6,23 +6,19 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Driver interface {
-	Drive(r *http.Request) error
-}
-
-type DriverImpl struct {
+type Driver struct {
 	Parser
 	Sender
 }
 
-func New(parser Parser, sender Sender) DriverImpl {
-	return DriverImpl{
+func New(parser Parser, sender Sender) Driver {
+	return Driver{
 		Parser: parser,
 		Sender: sender,
 	}
 }
 
-func (driver DriverImpl) Drive(r *http.Request) error {
+func (driver Driver) Drive(r *http.Request) error {
 	content, err := driver.Parser.Parse(r)
 	if err != nil {
 		return errors.Wrapf(err, "driver.Parser.Parse is failed. request is %v", r)
