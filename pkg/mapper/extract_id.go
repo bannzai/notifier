6ebebc2a71ff60de320e0985824090765d7f4754
bannzai <kingkong999yhirose@gmail.com>
2019-11-10 11:32:16 +0900

    :sparkles: Add strcure for mapping id

--- pkg/mapper/extract_id.go
+++ pkg/mapper/extract_id.go
@@ -6,11 +6,17 @@ import (
 	"github.com/bannzai/notifier/pkg/parser"
 )
 
+type Mapper struct{}
+
 func noMatchedError(idMapping IDMapping, content parser.Content, toContentType parser.ContentType) error {
 	return fmt.Errorf("Not matched id from content of %v, to %d, with mapping values %v", content, toContentType, idMapping)
 }
 
-func ExtractID(idMapping IDMapping, content parser.Content, toContentType parser.ContentType) (string, error) {
+func (Mapper) MapID(content parser.Content, toContentType parser.ContentType) (string, error) {
+	idMapping, err := fetchIDMap()
+	if err != nil {
+		return "", fmt.Errorf("fetchIDMap error %w", err)
+	}
 	switch content.ContentType {
 	case parser.GitHubMentionContent, parser.GitHubAssignedContent:
 		id := idMapping.extractFromGitHub(toContentType)
