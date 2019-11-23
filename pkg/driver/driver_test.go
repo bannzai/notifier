7a1ca4cc7616009870afc548bd6cbd8cf4404345
bannzai <kingkong999yhirose@gmail.com>
2019-11-23 22:58:53 +0900

    :recycle: Remove unnecessary test

--- pkg/driver/driver_test.go
+++ pkg/driver/driver_test.go
@@ -2,30 +2,8 @@ package driver
 
 import (
 	"net/http"
-	"reflect"
 	"testing"
 )
-
-func TestNew(t *testing.T) {
-	type args struct {
-		parser Parser
-		sender Sender
-	}
-	tests := []struct {
-		name string
-		args args
-		want Driver
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := New(tt.args.parser, tt.args.sender); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
 
 func TestDriver_Drive(t *testing.T) {
 	type fields struct {
