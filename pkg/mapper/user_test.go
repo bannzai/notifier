8ebb0837d0926067012923745a894fa1bea52be2
bannzai <kingkong999yhirose@gmail.com>
2019-11-23 20:53:29 +0900

    :shell: gotests -w -all pkg/mapper/user.go

--- pkg/mapper/user_test.go
+++ pkg/mapper/user_test.go
@@ -66,3 +66,23 @@ func Test_fetchUsers(t *testing.T) {
 		})
 	}
 }
+
+func Test_isRemoteURL(t *testing.T) {
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isRemoteURL(tt.args.path); got != tt.want {
+				t.Errorf("isRemoteURL() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
